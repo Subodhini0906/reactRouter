00938207f8d56f2f3040a13039f4c6499bdf1bed
Subodhini <subodhiniaggarwal@gmail.com>
2024-11-23 14:00:33 +0530

    added Header and Footer

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { RouterProvider,createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout />,
+    children:[]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
 )
